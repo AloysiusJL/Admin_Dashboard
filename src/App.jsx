f77e68d236e571e4472b4b29f33339ce3aa691f6
Aloysius Jeremy Laksono <aloysius.laksono@gmail.com>
2023-04-27 11:04:50 +0700

    add line page

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Calendar from "./scenes/calendar";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
+import Pie from "./scenes/pie";
 // import Form from "./scenes/form";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
+import Line from "./scenes/line";
+import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
@@ -36,10 +36,10 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
-              {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
